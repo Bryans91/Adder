83c7141e7ba1dbc814a64d833ccf95c89224dfa4
Bryan Schreuder <bryanschreuder91@gmail.com>
2020-05-23 15:49:25 +0200

    base

--- Adder/Components/Component.cs
+++ Adder/Components/Component.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Adder.Visitors;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,35 @@ namespace Adder.Components
 {
     public abstract class Component
     {
+
+        public TimeSpan TimeSpan { get; set; }
+        //protected IVisitor _visitor;
+
+        public virtual void Run(IVisitor visitor)
+        {
+            Stopwatch timer = new Stopwatch();
+            timer.Start();
+            
+
+            this.Accept(visitor);
+
+
+            timer.Stop();
+            this.TimeSpan = timer.Elapsed;
+            //this.PrintTime();
+        }
+
+
+     
+
+        public virtual void Accept(IVisitor visitor)
+        {
+            visitor.Visit(this);
+        }
+
+        public void PrintTime()
+        {
+            Console.WriteLine("Time elapsed (microseconds): {0}", TimeSpan.Ticks / 1000);
+        }
     }
 }
---
+++ Adder/Visitors/Validator.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Adder.Components;
+using Adder.Components.Nodes;
+
+namespace Adder.Visitors
+{
+    public class Validator : IVisitor
+    {
+        public void Visit(Component visited)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Visit(Circuit visited)
+        {
+            Console.WriteLine("this is a circuit");
+            //throw new NotImplementedException();
+        }
+
+        public void Visit(Node visited)
+        {
+            Console.WriteLine("this is a node");
+           // throw new NotImplementedException();
+        }
+
+        public void Visit(In visited)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Visit(Out visited)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Visit(And visited)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Visit(Nand visited)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Visit(Nor visited)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Visit(Not visited)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Visit(Or visited)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Visit(Xor visited)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
