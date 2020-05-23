032c492915878a120119f079c6ae3b20413334d4
Xyzer <xyzer.mail@gmail.com>
2020-05-23 16:30:58 +0200

    Added io, removed shit

---
+++ Adder/Factories/NodeFactory.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Adder.Components.Nodes;
+using Adder.Components;
+
+namespace Adder.Factories
+{
+    class NodeFactory
+    {
+		private Dictionary<string, Node> _prototypes = new Dictionary<string, Node>();
+
+        public NodeFactory()
+        {
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            _prototypes["AND"] = new And();
+            _prototypes["NOT"] = new Not();
+            _prototypes["NAND"] = new Nand();
+            _prototypes["NOR"] = new Nor();
+            _prototypes["OR"] = new Or();
+            _prototypes["PROBE"] = new Out();
+            _prototypes["INPUT"] = new In();
+            _prototypes["XOR"] = new Xor();
+        }
+
+        public void RegisterNode(string name, Node node)
+        {
+            _prototypes[name] = node;
+        }
+
+        public Node Create(String type)
+        {
+            Node prototype = _prototypes[type];
+
+            //return prototype.Clone();
+            return null;
+        }
+    }
+}
---
+++ Adder/IO/FileParser.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Adder.IO
+{
+    public class FileParser
+    {
+
+        public void ParseCircuit(String filePath)
+        {
+            FileReader fr = new FileReader(filePath);
+            Boolean readingEdges = false;
+            List<String> lines = fr.GetLines();
+
+            if (lines != null)
+            {
+                foreach (String line in lines)
+                {
+                    if (line.Equals("# Description of all the edges"))
+                    {
+                        readingEdges = true;
+                    }
+                    if ( ! line.StartsWith("#") && !String.IsNullOrEmpty(line))
+                    {
+                        if (line.Contains(":") && line.EndsWith(";"))
+                        {
+                            String[] circuitParts = GetCircuitParts(line);
+                            if (readingEdges)
+                            {
+                                AddEdges(circuitParts);
+                            }
+                            else
+                            {
+                                AddNode(circuitParts);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void AddNode(String[] nodeParts)
+        {
+            Console.WriteLine(nodeParts[0] + " has the following output:");
+            foreach(String part in nodeParts.Skip(1))
+            {
+                Console.WriteLine("- " + part);
+            }
+            Console.WriteLine("-------------------");
+        }
+
+        private void AddEdges(String[] edgeParts)
+        {
+            Console.WriteLine(edgeParts[0] + " has the following edges:");
+            foreach (String part in edgeParts.Skip(1))
+            {
+                Console.WriteLine("- " + part);
+            }
+            Console.WriteLine("-------------------");
+        }
+
+        private String[] GetCircuitParts(String line)
+        {
+            String pattern = @"([a-zA-Z0-9_]+)";
+            var result = Regex.Matches(line, pattern)
+                .OfType<Match>()
+                .Select(m => m.Groups[0].Value)
+                .ToArray();
+
+            return result;
+        }
+    }
+}
